73efdef6287210ac718fb52c234e2e4b24606e6c
alvarosg7 <alvarosg7@gmail.com>
2024-12-09 21:15:07 +0100

    resolved some loop challenges

--- python_practice.py
+++ python_practice.py
@@ -53,3 +53,40 @@ else:
 	print("Distinction")
 
 
+# Loops for and While 
+# Loops help you create actions 
+# for loop: iterate over a sequence (like a list or range)
+
+for i in range(5):
+	print("Iteration:", i)
+
+# while loop: continue as long as a condition is True
+
+count = 0
+
+while count < 3:
+	print("Count is,", count)
+	count += 1
+
+
+# Task 2: Write a program that:
+
+# Prints numbers from 1 to 10 using a for loop.
+# Prints numbers from 10 to 1 using a while loop.
+
+for i in range(1,11):
+	print(i)
+
+count_test = 10
+
+while count_test > 0: 
+	print(count_test)
+	count_test -= 1
+
+#Challenge Task:
+# Write a program that:
+
+# Prints all numbers from 1 to 20.
+# For each number:
+# Print "Even" if it's an even number.
+# Print "Odd" if it's an odd number.
