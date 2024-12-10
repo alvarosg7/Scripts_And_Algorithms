b92d910a2a7335689b0be2b243fdc4b2401ad307
alvarosg7 <alvarosg7@gmail.com>
2024-12-10 18:52:01 +0100

    Added file to practice data structures.

---
+++ data_structures.py
@@ -0,0 +1,15 @@
+
+
+#Step 4: Data Structures
+# Python has powerful built-in data structures:
+
+# Lists: Ordered, mutable collections.
+# Tuples: Ordered, immutable collections.
+# Dictionaries: Key-value pairs.
+# Sets: Unordered collections with no duplicates.
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # Access the first item
+fruits.append("orange")  # Add an item
+fruits.remove("banana")  # Remove an item
+print(fruits)
