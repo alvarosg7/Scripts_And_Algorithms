408c2b256b8eccd925ed10c27233d876d1ab8d0b
alvarosg7 <alvarosg7@gmail.com>
2024-12-16 22:25:28 +0100

    Make progress on palindrome checker

--- loop_and_condition_challenges.py
+++ loop_and_condition_challenges.py
@@ -7,3 +7,15 @@
 # A palindrome reads the same backward as forward (e.g., "madam", "racecar").
 # Ignore spaces and capitalization when checking.
 
+str = input("Enter your word here: ")
+
+str_list = list(str)
+
+reversed_str = []
+
+# print(str_list)
+
+for i in range(1,len(str_list) + 1):
+    reversed_str.append(str_list[len(str) - i])
+    
+print(reversed_str)
