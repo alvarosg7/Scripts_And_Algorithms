df099858df8c5864b86ab23c87f9ea186193789a
Alvaro <alvarosg7@gmail.com>
2024-12-18 14:41:51 +0100

    added new challenge for CTE

--- CTEs.sql
+++ CTEs.sql
@@ -26,3 +26,30 @@ WITH total_sales_per_product AS (
 
 SELECT *
 FROM total_sales_per_product
+
+
+/* Exercise 2: Filtering with a CTE
+Scenario:
+You have the following tables:
+
+employees:
+employee_id (INT)
+name (VARCHAR)
+department_id (INT)
+salary (DECIMAL)
+
+departments:
+department_id (INT)
+department_name (VARCHAR)
+Write a query using a CTE to:
+
+Calculate the average salary for each department.
+Find the employees who earn more than their department's average salary.
+
+Display:
+name
+department_name
+salary
+average_salary */
+
+
