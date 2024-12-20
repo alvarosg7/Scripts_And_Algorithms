feea5f521f22d4d275583a62ced2984257a4600e
alvarosg7 <alvarosg7@gmail.com>
2024-12-20 15:33:22 +0100

    Continued working on powershell

--- powershell_practice.ps1
+++ powershell_practice.ps1
@@ -1,2 +1,29 @@
 
 
+$PSVersionTable 
+
+# PowerShell lets you interact with your file system.
+
+Get-ChildItem
+
+# Alias: ls or dir
+
+# Change Directory
+
+Set-Location -Path "C:\Users\alvaro\Desktop\coding_practice"
+
+# Alias: cd
+
+# View current directory
+
+Get-Location 
+
+# Alias pwd
+
+# Powershell returns data as objects, not plain text. This allows you to manipulate and format data easily
+
+Get-Process
+
+# Each process is an object with properties like Name, Id, CPU, and more. You can view a specific property:
+
+(Get-Process -Name notepad).CPU
