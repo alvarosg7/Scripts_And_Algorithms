51b9364b11cea806769b972978fda7fde77cc1d9
Alvaro <alvarosg7@gmail.com>
2025-01-08 14:04:15 +0100

    resolved excersice on advanced powershell filtering

--- advanced_filtering.ps1
+++ advanced_filtering.ps1
@@ -1,33 +1,55 @@
 
 
 # Advanced Filtering
+
 # Find Idle Processes
- 
 # Use Get-Process to find processes consuming less than 1% CPU time (< 1).
-# Filter Services by Startup Type
- 
+
+get-process | Where-Object{$_.CPU -lt 1} 
+
+# Filter Services by Startup Type 
 # List all services that are set to start automatically.
-# Identify Stopped Services
- 
+
+Get-CimInstance -ClassName Win32_Service | Where-Object {$_.StartMode -eq 'Auto'}
+
+# Explanation
+
+# Get-CimInstance:
+# Retrieves detailed service information, including the startup type.
+# The Win32_Service class represents services on a Windows system.
+
+# Where-Object:
+# Filters based on the StartMode property.
+# StartMode -eq 'Auto': Selects services with a startup type of Automatic.
+
+# StartMode Values:
+# Auto: Automatic startup.
+# Manual: Manual startup.
+# Disabled: Disabled service.
+
+
+# Identify Stopped Services 
 # List all services that have the status Stopped.
-# Find Files Modified Recently
- 
+
+Get-Service | Where-Object{$_.Status -eq 'Stopped'}
+
+# Find Files Modified Recently 
 # Navigate to a directory and find all files modified within the last 7 days.
  
 
 # Bonus Challenges
-# Get System Uptime
- 
-# Find how long the system has been running using Get-WmiObject or another cmdlet.
-# List Top 3 Largest Files
- 
-# Navigate to a directory and list the top 3 largest files.
-# Monitor Processes Consuming High Memory
- 
-# Continuously monitor and display processes consuming more than 100 MB of memory.
-# Display Disk Usage
- 
-# Use a PowerShell cmdlet to display information about your system's drives (e.g., free space, total size).
-# Find Processes with Specific Properties
 
+# Get System Uptime 
+# Find how long the system has been running using Get-WmiObject or another cmdlet.
+
+# List Top 3 Largest Files 
+# Navigate to a directory and list the top 3 largest files.
+
+# Monitor Processes Consuming High Memory 
+# Continuously monitor and display processes consuming more than 100 MB of memory.
+
+# Display Disk Usage 
+# Use a PowerShell cmdlet to display information about your system's drives (e.g., free space, total size).
+
+# Find Processes with Specific Properties
 # Use Get-Process to display processes with specific properties (e.g., Name, CPU, and Id).
