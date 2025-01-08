cf93ab06a232aeaad3fad91803f3414a471da691
Alvaro <alvarosg7@gmail.com>
2025-01-08 11:05:21 +0100

    added advanced_filterin.ps1 file

---
+++ advanced_filtering.ps1
@@ -0,0 +1,33 @@
+
+
+# Advanced Filtering
+# Find Idle Processes
+ 
+# Use Get-Process to find processes consuming less than 1% CPU time (< 1).
+# Filter Services by Startup Type
+ 
+# List all services that are set to start automatically.
+# Identify Stopped Services
+ 
+# List all services that have the status Stopped.
+# Find Files Modified Recently
+ 
+# Navigate to a directory and find all files modified within the last 7 days.
+ 
+
+# Bonus Challenges
+# Get System Uptime
+ 
+# Find how long the system has been running using Get-WmiObject or another cmdlet.
+# List Top 3 Largest Files
+ 
+# Navigate to a directory and list the top 3 largest files.
+# Monitor Processes Consuming High Memory
+ 
+# Continuously monitor and display processes consuming more than 100 MB of memory.
+# Display Disk Usage
+ 
+# Use a PowerShell cmdlet to display information about your system's drives (e.g., free space, total size).
+# Find Processes with Specific Properties
+
+# Use Get-Process to display processes with specific properties (e.g., Name, CPU, and Id).
