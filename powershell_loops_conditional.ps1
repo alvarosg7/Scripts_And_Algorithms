878357045adf2a9a7e6374a209b93fff9577b7ae
Alvaro <alvarosg7@gmail.com>
2025-01-13 14:09:47 +0100

    Added first challenge for powershell conditional/loops

---
+++ powershell_loops_conditional.ps1
@@ -0,0 +1,7 @@
+
+
+# 1. Check Disk Space
+
+# Objective: Monitor a specific drive and alert if free space drops below a threshold. 
+# Prompt: Write a script that checks the free space on drive C: every 5 seconds. If the free space is less than 10 GB, display a warning message.
+
