bbab4e69d713508d21e932aa79d582e60647d22c
Alvaro <alvarosg7@gmail.com>
2025-01-15 13:22:55 +0100

    first PS conditional/loops challenge completed

--- powershell_loops_conditional.ps1
+++ powershell_loops_conditional.ps1
@@ -5,3 +5,10 @@
 # Objective: Monitor a specific drive and alert if free space drops below a threshold. 
 # Prompt: Write a script that checks the free space on drive C: every 5 seconds. If the free space is less than 10 GB, display a warning message.
 
+
+
+while ($true) {
+    $FreeSpace = Get-PSDrive -PSProvider FileSystem -Name C
+    if($FreeSpace.Free -lt 10GB){write-host "Warning, free space less than 10GB"} 
+    Start-Sleep -Seconds 5
+}
