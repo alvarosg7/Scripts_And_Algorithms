ff6cf6a2741ef36132197d6827ceb54c278ff1e3
Alvaro <alvarosg7@gmail.com>
2025-01-15 16:55:11 +0100

    added a few lines in the bash file, nothing fancy

--- bash.sh
+++ bash.sh
@@ -2,5 +2,21 @@
 
 # This is my first Bash script in WSL2
 
+
+echo "Script name: $0"
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Total arguments: $#" 
+
+
 echo 'Hello, Alvaro!'
 echo "Today's date is: $(date)"
+
+
+echo $USER
+
+fav_color='green' 
+echo $fav_color
+
+
+echo 'Hello ' $3 'You are ' $4 'years old' 
