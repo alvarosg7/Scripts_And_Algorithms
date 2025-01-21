8ecfe09d5e4919fe3c3d60c89904e936c32a78df
Alvaro <alvarosg7@gmail.com>
2025-01-21 10:22:55 +0100

    resolved another loop exercise

--- loops.sh
+++ loops.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+# The for loop iterates over a list of items, executing commands for each item.
+
 for name in Alice Bob Charlie; do
     echo "Hello, $name!"  
 done
@@ -9,3 +11,32 @@ done
 for file in /c/Users/Alvaro.Sandino/*; do
     echo "Found file: $file"
 done
+
+
+# The while loop executes a block of code as long as a condition is true
+
+
+# Write a script that uses a while loop to calculate the sum of numbers from 1 to a user-provided number
+
+#prompt the user for input
+
+read -p "Enter a number: " num
+
+
+# Initialize variables
+
+sum=0
+current=1
+
+# Loop to calculate sum
+
+while [ $current -le $num ]; do  
+    sum=$((sum + current)) # add current number to sum
+    current=$((current + 1)) # increment current number
+done
+
+# print result
+
+echo "The sum of numbers from 1 to $num is: $sum" 
+
+
